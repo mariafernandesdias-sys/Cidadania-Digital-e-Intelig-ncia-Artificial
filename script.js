edc442ac32e3a366d6aa99ef25146a5da11f0ce0
mariafernandesdias-sys <maria.fernandes.dias@escola.pr.gov.br>
2026-06-17 11:39:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function mostrarAlerta() {
-  alert("Educação digital e checagem de fontes são essenciais para combater deepfakes e desinformação!");
+  alert("A IA pode tanto informar quanto enganar. Sempre verifique fontes confiáveis!");
 }
